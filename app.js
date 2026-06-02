ed31b2f6f2aaef231b94bb0711d625cf20aa2f8c
Pankaj Jha <pankajjha7875@gmail.com>
2026-06-02 14:28:59 +0530

    Fix bug in payment processing

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.get('/stone-paper-scissors', (req, res) => {
 });
 
 // Database Connection (Mongoose Example)
-/*
+
 mongoose.connect('mongodb://localhost:27017/portfolio')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
-*/
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
