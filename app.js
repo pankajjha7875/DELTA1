c25fa8330967aee06844b6ff279f38c81738c5ce
Pankaj Jha <pankajjha7875@gmail.com>
2026-06-03 14:00:23 +0530

    Fix bugg

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/todo-game', (req, res) => {
 
 // Database Connection (Mongoose Example)
 
-mongoose.connect('mongodb://localhost:27017/portfolio')
+mongoose.connect('mongodb://127.0.0.1:27017/portfolio')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(err));
 
